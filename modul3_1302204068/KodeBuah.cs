ba7e26be544ac765055b4450a438ec5bf52ecd5f
vin14 <pasallimelvin@student.telkomuniversity.ac.id>
2022-03-12 12:19:45 +0700

    menambahkan class KodeBuah

---
+++ modul3_1302204068/KodeBuah.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul3_1302204068
+{
+    internal class KodeBuah
+    {
+        public enum Fruit
+        {
+            Apel, Aprikot, Alpukat, Pisang, Paprika, Blackberry, Ceri, Kelapa, Jagung, Kurma,
+            Durian, Anggur, Melon, Semangka
+        };
+
+        public String getKodeBuah(Fruit inputNamaBuah)
+        {
+            String[] arrayKodeNamaBuah = { "A00", "B00", "C00", "D00", "E00", "F00", "H00", "I00", "J00", "K00", "L00", "M00",
+            "N00", "O00" };
+
+            return arrayKodeNamaBuah[(int)inputNamaBuah];
+        }
+
+    }
+}
--- modul3_1302204068/Program.cs
+++ modul3_1302204068/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System;
+
+namespace modul3_1302204068
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KodeBuah objekBuah = new KodeBuah();
+
+            KodeBuah.Fruit inputNamaBuah = KodeBuah.Fruit.Pisang;
+
+            Console.WriteLine(objekBuah.getKodeBuah(inputNamaBuah));
+
+        }
+
+    }
+
+}
+
+
